b21d61f406f9f125d99d32c925344267d4183c8d
ryanrixxh <ryanmbr@protonmail.com>
2022-12-15 22:09:56 +1030

    setting full height of root div

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import './stylesheets/App.css';
 
 function App() {
   return (
-    <div className="App">
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="App bg-slate-500 h-screen">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
