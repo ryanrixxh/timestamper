da2fe4e9b3bf8e185fccc32008b7aab3d3e33378
ryanrixxh <ryanmbr@protonmail.com>
2022-12-21 00:16:53 +1030

    adding auth url

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import './stylesheets/App.css';
 import './components/Hotkey.tsx'
 import Hotkey from './components/Hotkey';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App bg-neutral-500 h-screen">
-      <div className="helloDiv">
-        <h1 className="text-5xl font-bold text-slate-300 hover:text-slate-200 text-center">
-          Login to Twitch
-        </h1>
-      </div>
+      <Login />
       <div className="createMarker">
         <h2 className="text-4xl font-bold text-slate-300 hover:text-slate-200 text-center">
           Create Marker
