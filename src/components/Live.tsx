83044d6777a0f67398af181b2cba1c25b8bf7237
ryanrixxh <ryanmbr@protonmail.com>
2022-12-22 00:43:52 +1030

    adding live icon

--- src/components/Live.tsx
+++ src/components/Live.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { getStatus, getToken } from "../utils/requests"
 import { User, Stream } from '../utils/interfaces'
+import LiveImage from '../assets/live-icon.png'
 
 let offline: Stream = {
     game_name: '',
@@ -30,16 +31,29 @@ function Live(user: User) {
     }, [token, user?.login])
 
     return (
-        <div className="liveDiv">
-            <h1 className="text-4xl font-bold text-slate-300 hover:text-slate-200 text-right">
-                { live }
-            </h1>
-            <h2 className="text-2xl font-bold text-slate-300 hover:text-slate-200 text-right">
-                Playing: { streamDetails?.game_name }
-            </h2>
-            <h2 className="text-2xl font-bold text-slate-300 hover:text-slate-200 text-right">
-                Title: { streamDetails?.title }
-            </h2>
+        <div className="liveDiv max-w-m">       
+            { live === 'Live' && 
+            <div>
+                <div className='flex flex-row-reverse'>
+                    <img src={LiveImage} alt='live icon' width='50' height='50'></img>
+                    <h1 className="text-4xl font-bold text-slate-300 hover:text-slate-200 text-right">
+                        { live }
+                    </h1>
+                </div>
+                <h2 className="text-2xl font-bold text-slate-300 hover:text-slate-200 text-right">
+                    Playing: { streamDetails?.game_name }
+                </h2> 
+                <h2 className="text-m font-bold text-slate-300 hover:text-slate-200 text-right">
+                    Title: { streamDetails?.title }
+                </h2>
+            </div>
+            } 
+            { live === 'Offline' &&
+                <h1 className="text-4xl font-bold text-slate-300 hover:text-slate-200 text-right">
+                    { live }
+                </h1> 
+            }
+
         </div>
     )
 }
