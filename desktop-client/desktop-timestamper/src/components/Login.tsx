fcd285afc86cb973a1c7aa6eadd23c8f68c3f92c
ryanrixxh <ryanmbr@protonmail.com>
2022-12-24 03:54:29 +1030

    shell url load now works. just needs a deep link

--- desktop-client/desktop-timestamper/src/components/Login.tsx
+++ desktop-client/desktop-timestamper/src/components/Login.tsx
@@ -3,7 +3,7 @@
 //Authorization URL
 function buildURL(): string {
     const state = uuid()
-    var href = 'https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=v89m5cded20ey1ppxxsi5ni53c3rv0&redirect_uri=http://localhost:3000/logged&scope=channel%3Amanage%3Abroadcast&state=1'
+    var href = 'https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=v89m5cded20ey1ppxxsi5ni53c3rv0&redirect_uri=http://localhost:9000&scope=channel%3Amanage%3Abroadcast&state=1'
     href += state
     return href
 }
@@ -14,7 +14,7 @@ function Login() {
     return (
         <div className="helloDiv">
             <h1 id="loginHeader" className="text-5xl font-bold text-slate-300 hover:text-slate-200 text-center">
-                <a href={link} >Login to Twitch</a>
+                <a href={link} target="_blank" rel="noopener noreferrer">Login to Twitch</a>
             </h1>
         </div>
     )
