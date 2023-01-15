e7a08a945628dbe50357adbcbf2c4d86033c3e89
ryanrixxh <ryanmbr@protonmail.com>
2023-01-15 23:10:49 +1030

    token returned to frontend successfully

--- tauri/src-tauri/src/main.rs
+++ tauri/src-tauri/src/main.rs
@@ -1,5 +1,10 @@
 use tauri::{Manager, AppHandle, Window};
 use url::Url;
+use std::thread;
+use std::thread::sleep;
+use core::time::Duration;
+use std::cell::RefCell;
+use std::sync::{Arc, RwLock};
 
 mod navigate;
 
@@ -24,34 +29,53 @@ struct Payload {
 
 
 #[tauri::command]
-async fn twitch_auth_flow(app: AppHandle) {
+async fn twitch_auth_flow(app: AppHandle) -> String {
   //Build a new window to handle the auth flow
+  let token = Arc::new(RwLock::new("empty".to_string()));
+  let token_clone = token.clone();
+  let token_read = token.clone();
+
   let window = tauri::WindowBuilder::new(&app, "twitch-auth", tauri::WindowUrl::App("../../index.html".into()))
       //When window navigates, capture url
       .on_navigation(move |url: url::Url| {
         let str = url.as_str();
         //Grab token from return url
         if str.starts_with(TWITCH_REDIRECT_URL) {
-          let url_vec = str.split("=").collect::<Vec<&str>>();
-          let token = url_vec[1].split("&").collect::<Vec<&str>>()[0];
-          println!("Token: {}", token);
+          if let Ok(mut result) = token_clone.write() {
+            let url_vec = str.split("=").collect::<Vec<&str>>();
+            *result = url_vec[1].split("&").collect::<Vec<&str>>()[0].to_string();
+            println!("Token: {}", *result);
+          }
         }
         true
       })
       .build()
       .unwrap();
   
-  window.hide();
-    
   //Navigate to auth url
   let auth = Url::parse(TWITCH_AUTH_URL).unwrap();
   navigate::webview_navigate(&window, auth).unwrap();
+  
+  //Wait for recieved token
+  let mut recieved = false;
+  while recieved == false {
+    if let Ok(read_token) = token_read.read() {
+      if *read_token != "empty" {
+        recieved = true;
+        return (*read_token.clone()).to_string().into();
+      }
+    }
+    sleep(Duration::from_millis(500));
+  }
+
+  return "failed_to_receive".to_string();
 }
 
 fn main() {
   tauri::Builder::default()
     .setup(|app| {
       let window = app.get_window("main").unwrap();
+      window.open_devtools();
       Ok(())
     })
     .invoke_handler(tauri::generate_handler![twitch_auth_flow])
--- tauri/src/Login.tsx
+++ tauri/src/Login.tsx
@@ -2,21 +2,26 @@ import { useState, useEffect } from 'react'
 import { emit, listen } from '@tauri-apps/api/event'
 import { invoke } from '@tauri-apps/api/tauri'
 
-async function auth() {
-  await invoke('twitch_auth_flow')
-}
 
 function Login() {
-  console.log('JS is printing!')
-  listen('hello-event', (event) => {
-    console.log(event)
-  })
+  
+  //Recieve token from Rust backend and set
+  //TODO: This may actually not be needed. Depends if we wanna keep invoking rust to handle the http requests or just use React. 
+  //Look at performace to see which will be better. 
+  async function auth() {
+    await invoke('twitch_auth_flow').then((message) => {
+      console.log("Rust sent: ", message)
+      setToken(message as any)
+    }
+  )}
+
+  const [token, setToken] = useState("empty")
 
   return (
     <div className="App">
       <div className="card">
-        <button onClick={async () => { invoke('twitch_auth_flow') }}>
-          Login
+        <button onClick={auth}>
+          Login + {token}
         </button>
       </div>
     </div>
