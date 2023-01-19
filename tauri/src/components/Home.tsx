c1547f5a153f5caad5c8e551d7d2b41b1dbc2f00
ryanrixxh <ryanmbr@protonmail.com>
2023-01-20 00:02:10 +1030

    moved websocket to component

--- tauri/src/components/Home.tsx
+++ tauri/src/components/Home.tsx
@@ -1,9 +1,40 @@
 import { useState, useEffect, useReducer } from 'react'
-import { createClient, getUserData, createLiveListener } from '../utils/api'
+import { createClient, getUserData, postEventSub } from '../utils/api'
 import { User, Stream } from '../utils/interfaces'
 
 function Home(props) {
   const [user, setUser] = useState<User>()
+  const [stream, setStream] = useState<Stream>()
+
+  // Websocket to listen for changes in stream status
+  
+  async function createLiveListener(id?: string) {
+    let ws_id
+    const socket = new WebSocket('wss://eventsub-beta.wss.twitch.tv/ws')
+    //needs a check to see if the message is a welcome or subscription message
+    socket.onmessage = (event) => {
+      const message = JSON.parse(event.data)
+      if (message.metadata.message_type === 'session_welcome') {
+          ws_id = message.payload.session.id
+  
+          const body: any = {
+              "type": "channel.update",
+              "version": "1",
+              "condition": {
+                  "broadcaster_user_id": id
+              },
+              "transport": {
+                  "method": "websocket",
+                  "session_id": ws_id,
+              }
+          }
+        postEventSub(body)
+      } else if (message.metadata.message_type !== 'session_keepalive') {
+          console.log(message.payload.event)
+          setStream(message.payload.event)
+      }
+    }
+  }
 
   useEffect(() => { 
     createClient(props.token)
@@ -28,6 +59,7 @@ function Home(props) {
         <h1 className="text-3xl font-bold">
           Hello {user?.display_name}
         </h1>
+        <h2> Your current title is: {stream?.title}</h2>
       </div>
     </div>
   )
--- tauri/src/utils/api.ts
+++ tauri/src/utils/api.ts
@@ -14,34 +14,13 @@ export function createClient(token_input) {
   })
 }
 
-export async function createLiveListener(id?: string) {
-  let ws_id
-  const socket = new WebSocket('wss://eventsub-beta.wss.twitch.tv/ws')
-  //needs a check to see if the message is a welcome or subscription message
-  socket.onmessage = (event) => {
-    const message = JSON.parse(event.data)
-    if (message.metadata.message_type === 'session_welcome') {
-        ws_id = message.payload.session.id
 
-        const body: any = {
-            "type": "stream.online",
-            "version": "1",
-            "condition": {
-                "broadcaster_user_id": id
-            },
-            "transport": {
-                "method": "websocket",
-                "session_id": ws_id,
-            }
-        }
-        client.post('/eventsub/subscriptions', body)
-    } else {
-        console.log(message)
-    }
-  }
-}
 
 export async function getUserData() {
   const response = await client.get('/users')
   return response.data.data[0]
+}
+
+export async function postEventSub(body: any) {
+  client.post('/eventsub/subscriptions', body)
 }
