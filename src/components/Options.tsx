cd97be4191f62ca349ae7d4b03d2cd00e700eeed
ryanrixxh <ryanmbr@protonmail.com>
2023-06-17 22:34:34 +0930

    adding useeffect for options panel

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react'
+
 import '../styles/options.css'
 
 function Options(props) {
@@ -6,6 +8,10 @@ function Options(props) {
         await props.store.set(name, { value: event.target.checked})
     }
 
+    useEffect(() => {
+        console.log('checking for options values')
+    },[])
+
     return (
         <div>
             <div className='heading'>Options</div>
@@ -13,17 +19,17 @@ function Options(props) {
                  <ul id='optionsList'>
                     <li className='option'>
                         <input className='optionToggle' 
-                               onChange={(event) => saveOption(event, 'darkmode')} 
+                               onChange={(event) => saveOption(event, 'option-darkmode')} 
                                type="checkbox" 
                                name="darkmode" />
                         <p className='optionName'>Dark mode (available soon)</p>  
                     </li>
                     <li className='option'>
                         <input className='optionToggle' 
-                               onChange={(event) => saveOption(event, 'localsave')} 
+                               onChange={(event) => saveOption(event, 'option-localsave')} 
                                type="checkbox" 
                                name="localsave" />
-                        <p>Save markers locally while online</p>
+                        <p className='optionName'>Save markers locally while online</p>
                     </li>
                  </ul>
             </div>
